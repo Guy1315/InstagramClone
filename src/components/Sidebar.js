856e388c704cccd7d870620fb878daeb772ada77
Guy1315 <98638942+Guy1315@users.noreply.github.com>
2023-11-11 17:43:22 +0200

    Add files via upload

---
+++ src/components/Sidebar.js
@@ -0,0 +1,28 @@
+import "../styles/sidebar.css";
+import Sticky from "react-sticky-el";
+import Profile from "./Profile";
+import Suggestions from "./Suggestions";
+import Footer from "./Footer";
+import image from "../images/profile.jpg";
+
+function Sidebar() {
+  return (
+    //the sticky enables the sidebar to stick to the top of the screen
+    //when your scroll reaches to the specified position
+    <Sticky topOffset={-80}>
+      <div className="sidebar">
+        <Profile
+          username="aleks.popovic"
+          caption="Aleksandar Popović"
+          urlText="Switch"
+          iconSize="big"
+          image={image}
+        />
+        <Suggestions />
+        <Footer />
+      </div>
+    </Sticky>
+  );
+}
+
+export default Sidebar;
